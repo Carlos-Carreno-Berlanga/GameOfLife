0a537ea03465c9efcfd9bcbebb3a484fbe13fc3c
Carlos <carcarbe@gmail.com>
2018-10-21 20:01:18 +0200

    Save game status in Redux.

---
+++ GameOfLife/ClientApp/src/components/GameOfLifePage.js
@@ -0,0 +1,45 @@
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+
+import { actionCreators } from '../store/GameOfLifePage';
+let hubConnection = null;
+class GameOfLifePage extends Component {
+    constructor() {
+        super();
+        this.handleNotification();
+    }
+
+    handleNotification() {
+        if (hubConnection) {
+            console.log("unsuscribe");
+            hubConnection.off("Notify");
+            hubConnection = null;
+        }
+
+        hubConnection = new HubConnectionBuilder()
+            .withUrl('/GameNotifier')
+            .build();
+
+        hubConnection.on("Notify", (data) => {
+            console.log("Notify", data);
+            this.props.receiveGameStatus(data);
+
+        });
+        hubConnection.start();
+    }
+
+    render() {
+        return (
+            <div>HOLA</div>
+        );
+    }
+
+
+}
+
+export default connect(
+    state => state.gameOfLife,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(GameOfLifePage);
---
+++ GameOfLife/ClientApp/src/store/GameOfLifePage.js
@@ -0,0 +1,19 @@
+﻿const receiveGameStatusType = 'RECEIVE_GAME_STATUS';
+const initialState = { gameStatus: null };
+
+export const actionCreators = {
+    receiveGameStatus: (gameStatus) => ({ type: receiveGameStatusType, gameStatus })
+};
+
+export const reducer = (state, action) => {
+    state = state || initialState;
+
+    if (action.type === receiveGameStatusType) {
+        return {
+            ...state,
+            gameStatus: action.gameStatus
+        };
+    }
+
+    return state;
+};
